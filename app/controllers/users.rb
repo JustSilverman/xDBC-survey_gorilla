ee766abfacc752a91848de4716b73d82545731d1
Apprentice <apprentice@devbootcamp.com>
2013-03-01 18:33:25 -0800

    play with colors not happy

---
+++ app/controllers/users.rb
@@ -0,0 +1,43 @@
+enable :sessions
+
+get '/' do
+  redirect '/home'
+end 
+
+get '/home' do 
+  current_user ? erb(:all_surveys_show) : erb(:login_signup)
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :user_show
+end 
+
+post '/signup' do
+  @user = User.new params
+  if @user.save
+    login(@user.id)
+    redirect("/users/#{@user.id}")
+  else 
+    erb :login_signup
+  end
+end 
+
+post '/login' do
+  @user = User.find_by_email(params[:email])
+  if @user && @user.authenticate(params[:password])
+    login(@user.id)
+    redirect "/users/#{@user.id}"
+  else 
+    redirect '/home'
+  end 
+end 
+
+delete '/logout' do  
+  logout 
+  redirect '/home'
+end
+
+
+
+
--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
   var questionCount = 1;
   //add a question with 3 answers
-  $('#add_question').click(function(){
+  $(this).on("click", '#add_question', function(){
     questionCount++;
     var questionAnswers = '<p><label for="question'+questionCount+'">Question '+questionCount+': </label><input type="text" name="question'+questionCount+'" placeholder="Enter your Question" id="question'+questionCount+'"><span class="removeQuestion"> Remove Question</span><br><input type="text" name="answer1" placeholder="answer 1"><br><input type="text" name="answer2" placeholder="answer 2"><br><input type="text" name="answer3" placeholder="answer 3"></p>'
     $(this).parent().before(questionAnswers); 
   });
   //remove a question and its answers  
   $(this).on("click", '.removeQuestion', function(){
-    
      $(this).parent().remove();
      questionCount--;
   });
