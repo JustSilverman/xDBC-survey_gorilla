f20012c7b48ada258d8eec473f9d9d4022085dc6
Justin Silverman <justin.silverman@gmail.com>
2013-03-03 12:29:35 -0800

    Survey display in process

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,6 +7,18 @@ class Question < ActiveRecord::Base
 
   before_save { self.content.strip! }
 
+  def total_responders
+    Selection.where(:response_id => self.choice_ids).count
+  end
+
+  def choice_percentage(choice_id)
+    ((Choice.find(choice_id).selections.count / self.total_responders.to_f) * 100).round(1)
+  end
+
+  def sorted_choices
+    # self.choices.sort_by { |choice| 1 / choice_percentage(choice.id) }
+  end
+
   def add_choices(choices)
     choices.each { |choice| self.choices.build(:content => choice) }
   end
